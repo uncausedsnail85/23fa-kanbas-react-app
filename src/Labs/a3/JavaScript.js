4b66a2daad6c1d80b3479d04a53b1161d861623d
Bryan Ang <bryan.ang>
2023-10-11 00:25:50 -0700

    lab 3 2.2.6.3

---
+++ src/Labs/a3/JavaScript.js
@@ -0,0 +1,23 @@
+import VariableTypes from "./VariableTypes";
+import VariablesAndConstants from "./VariablesAndConstants";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import WorkingWithFunctions from "./WorkingWithFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+
+function JavaScript() {
+    return(
+       <div>
+          <h1>JavaScript</h1>
+          <VariablesAndConstants/>
+          <VariableTypes/>
+          <BooleanVariables />
+          <IfElse />
+          <TernaryOperator />
+          <WorkingWithFunctions />
+          <ImpliedReturn />
+       </div>
+    );
+ }
+ export default JavaScript
