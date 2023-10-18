a0b8c7e8ad5f5dd4fc40cb6a5af185dec1d6b164
Bryan Ang <bryan.ang>
2023-10-18 00:09:40 -0700

    a3 lab

--- src/Labs/a3/JavaScript.js
+++ src/Labs/a3/JavaScript.js
@@ -5,6 +5,21 @@ import IfElse from "./IfElse";
 import TernaryOperator from "./TernaryOperator";
 import WorkingWithFunctions from "./WorkingWithFunctions";
 import ImpliedReturn from "./ImpliedReturn";
+import FunctionParenthesisAndParameters from "./FunctionParenthesisAndParameters";
+import WorkingWithArrays from "./WorkingWithArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingDataToFromArrays from "./AddingAndRemovingDataToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import JsonStringify from "./JsonStringify";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+import TemplateLiterals from "./TemplateLiterals";
+import House from "./House";
+import Spread from "./Spread";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
 
 function JavaScript() {
     return(
@@ -17,6 +32,21 @@ function JavaScript() {
           <TernaryOperator />
           <WorkingWithFunctions />
           <ImpliedReturn />
+          <FunctionParenthesisAndParameters />
+          <WorkingWithArrays />
+          <ArrayIndexAndLength />
+          <AddingAndRemovingDataToFromArrays />
+          <ForLoops />
+          <MapFunction />
+          <JsonStringify />
+          <FindFunction />
+          <FindIndex />
+          <FilterFunction />
+          <TemplateLiterals />
+          <House />
+          <Spread />
+          <Destructing />
+          <FunctionDestructing />
        </div>
     );
  }
