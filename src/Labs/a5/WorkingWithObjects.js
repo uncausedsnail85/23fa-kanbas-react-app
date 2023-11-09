d9d775870915b973c39afefd81671a0f6c34df12
Bryan Ang <bryan.ang>
2023-11-08 17:39:18 -0800

    lab 3.2

---
+++ src/Labs/a5/WorkingWithObjects.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+
+function WorkingWithObjects() {
+    const [assignment, setAssignment] = useState({
+        id: 1,
+        title: "NodeJS Assignment",
+        description: "Create a NodeJS server with ExpressJS",
+        due: "2021-10-10",
+        completed: false,
+        score: 0,
+    });
+    const URL = "http://localhost:4000/a5/assignment"
+    
+    return (
+        <div>
+            <h3>Working With Objects</h3>
+            <h4>Modifying Properties</h4>
+            <a
+                href={`${URL}/title/${assignment.title}`}
+                className="btn btn-primary me-2 float-end"
+            >
+                Update Title
+            </a>
+            <input
+                onChange={(e) => setAssignment({
+                    ...assignment,
+                    title: e.target.value
+                })}
+                value={assignment.title}
+                className="form-control mb-2 w-75"
+                type="text" />
+
+            <a
+                href={`${URL}/score/${assignment.score}`}
+                className="btn btn-primary me-2 float-end"
+            >
+                Update Score
+            </a>
+            <input
+                onChange={(e) => setAssignment({
+                    ...assignment,
+                    score: e.target.value
+                })}
+                value={assignment.score}
+                className="form-control mb-2 w-75"
+                type="number" />
+
+            <a
+                href={`${URL}/completed/${assignment.completed}`}
+                className="btn btn-primary me-2 float-end"
+            >
+                Update Completed
+            </a>
+            <input
+                onClick={() => {
+                    var newCompleted = !assignment.completed
+                    setAssignment({
+                    ...assignment,
+                    completed: newCompleted
+                })}}
+                checked={assignment.completed}
+                className="form-check-input"
+                id="completed"
+                type="checkbox" />
+            <label className="form-check-label" for="completed">
+                Completed?
+            </label>
+
+            <h4>Retrieving Objects</h4>
+            <a href="http://localhost:4000/a5/assignment"
+                className="btn btn-primary me-2">
+                Get Assignment
+            </a>
+            <h4>Retrieving Properties</h4>
+            <a
+                href="http://localhost:4000/a5/assignment/title"
+                className="btn btn-primary me-2">
+                Get Title
+            </a>
+        </div>
+    );
+}
+export default WorkingWithObjects;
--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,6 +1,7 @@
 import Nav from "./a3/Nav";
 import Assignment3 from "./a3";
 import Assignment4 from "./a4";
+import Assignment5 from "./a5";
 import { Routes, Route, Navigate }
   from "react-router";
 import store from "./store";
@@ -19,6 +20,8 @@ function Labs() {
             element={<Assignment3 />} />
           <Route path="a4"
             element={<Assignment4 />} />
+          <Route path="a5"
+            element={<Assignment5 />} />
         </Routes>
       </Provider>
     </div>
